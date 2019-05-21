5cd609b4ff1881143a19bcf76dab3f347934347f
wildmanpeace <alex.ladell.wild@gmail.com>
2019-05-20 23:01:53 -0600

    Added a counterReset Function and constructor if we have a  name for the counter.

--- side_project/lib/counterClass.dart
+++ side_project/lib/counterClass.dart
@@ -1,13 +1,35 @@
 import 'dart:core';
 
+class Counter {
+  int count;
+  DateTime lastUpdateTime;
+  String counterName;
 
-class Counter{
- int count;
-    Counter(){
-      this.count = 0;
-    }
+  //default
+  Counter() {
+    this.count = 0;
+    this.lastUpdateTime = DateTime.now();
+    this.counterName = "";
+  }
 
-    int increaseCounter(Counter counter){
-        counter.count++;
+//with name
+Counter.counterWithName(String name){
+  this.count = 0;
+    this.lastUpdateTime = DateTime.now();
+    this.counterName = name;
+}
+
+  int increaseCounter(Counter counter) {
+    counter.count++;
+    counter.lastUpdateTime = DateTime.now();
+  }
+
+  void counterReset(Counter counter) {
+    var counterUpdate = DateTime.now()
+        .difference(counter.lastUpdateTime);
+
+    if (counterUpdate.inHours > 36) {
+      counter.count = 0;
     }
+  }
 }
